a9620a19153033bc095a8f87158c917111d721c1
@hexlet/cli <support@hexlet.io>
2023-06-04 04:18:15 +0300

    submit java-spring-ru/intro-to-spring

--- java-spring-ru/intro-to-spring/src/main/java/exercise/WelcomeController.java
+++ java-spring-ru/intro-to-spring/src/main/java/exercise/WelcomeController.java
@@ -8,16 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
     @GetMapping("/")
-    // Обработчик
-    // Привязываем параметр запроса к параметру метода и задаём значение по умолчанию
     public String welcome() {
         return "Welcome to Spring";
     }
 
-    // Сопоставляем GET запрос на адрес /users с обработчиком
     @GetMapping("/hello")
-    // Обработчик
-    // Привязываем параметр запроса к параметру метода и задаём значение по умолчанию
     public String helloUser(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello, %s!", name);
     }
